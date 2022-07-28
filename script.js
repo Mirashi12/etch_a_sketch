0aa91446b212671755ceb0093aec6621dea70f0e
jm <rqt87000@gmail.com>
2022-07-28 22:10:22 +0200

    Add prompt to decide the size of the canva + code to remove existing canva

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 let square_numbers = 0;
 let grid_container = document.querySelector(".drawing-area");
 let empty_squares;
-
-let btn_16 = document.querySelector('#btn-16');
-let btn_50 = document.querySelector('#btn-50');
+let btn_grid = document.querySelector('#btn-grid');
 
 function createGrid () {
-    square_numbers = this.square_numbers
-    for (let i = 0; i < square_numbers; i++) {
-        let divs = document.createElement('div');
-        grid_container.appendChild(divs);
-        divs.setAttribute('class', 'empty-square');
-        divs.addEventListener('mouseover', function(){
-            divs.setAttribute('id', 'drawing-square');
-        });
+    let input_number = prompt("How many squares? 100 max");
+    square_numbers = input_number * input_number; // Number of pixels in the square
+    let canva_size = input_number * 10; // Determine the size of the canva, with each pixel being 10px squares
+    if (input_number <= 100) { // limit the consumption of resources
+    grid_container.style.height = canva_size + 'px';
+    grid_container.style.width = canva_size + 'px';
+    
+    let existing_grid = document.querySelectorAll('.empty-square') // Selects and removes all the component of the existing canva
+    existing_grid.forEach( element => element.remove());
+
+        for (let i = 0; i < square_numbers; i++) {
+            let divs = document.createElement('div'); // Creates a square with the necessary amount of div elements
+            grid_container.appendChild(divs);
+            divs.setAttribute('class', 'empty-square');
+            divs.addEventListener('mouseover', function(){ // Creates a function which will turn the div elements black on mouseover
+                divs.setAttribute('id', 'drawing-square');
+            });
+        }
+    } else {
+        alert("Please select a number inferior to 100")
     }
 }
 
-btn_16.square_numbers = 16 * 16;
-btn_16.addEventListener('click', createGrid);
-
-btn_50.square_numbers = 50 * 50;
-btn_50.addEventListener('click', createGrid);
+btn_grid.addEventListener('click', createGrid);
